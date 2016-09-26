ddb282ea4a118c9c7437eec3fd0c10d63ac6cecc
onlythenaive <ilya.gubarev@gmail.com>
2016-09-26 15:07:59 +0300

    patch/point entities mapped

--- src/main/java/ru/spb/iac/storager/server/data/patches/Patch.java
+++ src/main/java/ru/spb/iac/storager/server/data/patches/Patch.java
@@ -1,26 +1,74 @@
 package ru.spb.iac.storager.server.data.patches;
 
-import ru.spb.iac.storager.server.data.points.Point;
-import ru.spb.iac.storager.server.data.providers.Provider;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import java.time.Instant;
 import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import ru.spb.iac.storager.server.data.points.Point;
+import ru.spb.iac.storager.server.data.providers.Provider;
 
 @Entity
 @Table(name = "patches")
 public class Patch {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
     private Integer id;
 
-    private Provider provider;
-
+    @Column(name = "comment")
     private String comment;
 
+    @Column(name = "created_at")
     private Instant createdAt;
 
+    @Column(name = "status")
     private String status;
 
+    @OneToMany(mappedBy = "patch")
     private List<Point> points;
+
+    @ManyToOne
+    @JoinColumn(name = "provider_id")
+    private Provider provider;
+
+    public Integer getId () {
+        return id;
+    }
+
+    public String getComment () {
+        return comment;
+    }
+
+    public void setComment (final String comment) {
+        this.comment = comment;
+    }
+
+    public Instant getCreatedAt () {
+        return createdAt;
+    }
+
+    public String getStatus () {
+        return status;
+    }
+
+    public List<Point> getPoints () {
+        return points;
+    }
+
+    public Provider getProvider () {
+        return provider;
+    }
+
+    public void setProvider (final Provider provider) {
+        this.provider = provider;
+    }
 }
--- src/main/java/ru/spb/iac/storager/server/data/points/Point.java
+++ src/main/java/ru/spb/iac/storager/server/data/points/Point.java
@@ -1,28 +1,98 @@
 package ru.spb.iac.storager.server.data.points;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import ru.spb.iac.storager.server.data.indicators.Indicator;
 import ru.spb.iac.storager.server.data.patches.Patch;
 import ru.spb.iac.storager.server.data.periods.Period;
 import ru.spb.iac.storager.server.data.territories.Territory;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
 @Entity
 @Table(name = "points")
 public class Point {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
     private Integer id;
 
+    @Column(name = "real")
     private Double real;
 
+    @Column(name = "plan")
     private Double plan;
 
+    @ManyToOne
+    @JoinColumn(name = "indicator_id")
     private Indicator indicator;
 
+    @ManyToOne
+    @JoinColumn(name = "patch_id")
     private Patch patch;
 
+    @ManyToOne
+    @JoinColumn(name = "period_id")
     private Period period;
 
+    @ManyToOne
+    @JoinColumn(name = "territory_id")
     private Territory territory;
+
+    public Integer getId () {
+        return id;
+    }
+
+    public Double getReal () {
+        return real;
+    }
+
+    public void setReal (final Double real) {
+        this.real = real;
+    }
+
+    public Double getPlan () {
+        return plan;
+    }
+
+    public void setPlan (final Double plan) {
+        this.plan = plan;
+    }
+
+    public Indicator getIndicator () {
+        return indicator;
+    }
+
+    public void setIndicator (final Indicator indicator) {
+        this.indicator = indicator;
+    }
+
+    public Patch getPatch () {
+        return patch;
+    }
+
+    public void setPatch (final Patch patch) {
+        this.patch = patch;
+    }
+
+    public Period getPeriod () {
+        return period;
+    }
+
+    public void setPeriod (final Period period) {
+        this.period = period;
+    }
+
+    public Territory getTerritory () {
+        return territory;
+    }
+
+    public void setTerritory (final Territory territory) {
+        this.territory = territory;
+    }
 }
