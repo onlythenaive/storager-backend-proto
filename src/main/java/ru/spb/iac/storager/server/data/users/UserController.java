571a4b702fdf7fe4436e205eb53383179f8717bb
IlyaGubarev <ilya.gubarev@gmail.com>
2016-09-26 04:57:26 +0400

    users route changed

--- src/main/java/ru/spb/iac/storager/server/data/users/UserController.java
+++ src/main/java/ru/spb/iac/storager/server/data/users/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/data/users")
+@RequestMapping("/data/security/users")
 public class UserController {
 
     @Autowired
