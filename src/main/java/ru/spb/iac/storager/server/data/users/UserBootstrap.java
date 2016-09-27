ce760575c695b5cca21b5ce98ed3099dfdec4e7f
IlyaGubarev <ilya.gubarev@gmail.com>
2016-09-28 03:05:56 +0400

    guest user is not root, whitelabel error page removed

--- src/main/java/ru/spb/iac/storager/server/data/users/UserBootstrap.java
+++ src/main/java/ru/spb/iac/storager/server/data/users/UserBootstrap.java
@@ -13,7 +13,7 @@ public class UserBootstrap {
 
     public void run() {
         userRepository.save(ImmutableList.of(
-                User.of("guest", "guest", "guest@storager.iac.spb.ru", "Гостевой доступ", true, true, "GUEST"),
+                User.of("guest", "guest", "guest@storager.iac.spb.ru", "Гостевой доступ", true, false, "GUEST"),
                 User.of("user", "user123", "user@storager.iac.spb.ru", "Юзеров Ю.Ю.", true, false, "USER"),
                 User.of("admin", "admin123", "admin@storager.iac.spb.ru", "Админов А.А.", true, false, "USER ADMIN")
         ));
--- src/main/java/ru/spb/iac/storager/server/security/NotAuthorizedException.java
+++ src/main/java/ru/spb/iac/storager/server/security/NotAuthorizedException.java
@@ -1,5 +1,9 @@
 package ru.spb.iac.storager.server.security;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
 public class NotAuthorizedException extends RuntimeException {
 
 }
