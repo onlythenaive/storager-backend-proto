078c8de9dc2120932bc10b8b4fd3348643a2732b
IlyaGubarev <ilya.gubarev@gmail.com>
2016-10-01 16:52:07 +0400

    territory bootstrap improved

--- src/main/java/ru/spb/iac/storager/server/domain/territories/TerritoryBootstrap.java
+++ src/main/java/ru/spb/iac/storager/server/domain/territories/TerritoryBootstrap.java
@@ -11,17 +11,33 @@ import org.springframework.transaction.annotation.Transactional;
 public class TerritoryBootstrap {
 
     @Autowired
-    private TerritoryService territoryService;
+    private TerritoryService service;
 
     public void run() {
-        territoryService.create(generate("1", null, "Территория 1", null));
-        territoryService.create(generate("1.1", "1", "Территория 1.1", null));
+        service.create(properties("1", null, "Территория 1", "Верхний уровень"));
+        service.create(properties("1.1", "1", "Территория 1.1", "Первый уровень вложенности, дочерняя для 1"));
+        service.create(properties("1.2", "1", "Территория 1.2", "Первый уровень вложенности, дочерняя для 1"));
+        service.create(properties("1.3", "1", "Территория 1.3", "Первый уровень вложенности, дочерняя для 1"));
+        service.create(properties("1.3.1", "1.3", "Территория 1.3.1", "Второй уровень вложенности, дочерняя для 1.3"));
+        service.create(properties("1.3.2", "1.3", "Территория 1.3.2", "Второй уровень вложенности, дочерняя для 1.3"));
+        service.create(properties("1.3.3", "1.3", "Территория 1.3.3", "Второй уровень вложенности, дочерняя для 1.3"));
+        service.create(properties("1.4", "1", "Территория 1.4", "Первый уровень вложенности, дочерняя для 1"));
+        service.create(properties("2", null, "Территория 2", "Верхний уровень"));
+        service.create(properties("2.1", "2", "Территория 2.1", "Первый уровень вложенности, дочерняя для 2"));
+        service.create(properties("2.2", "2", "Территория 2.2", "Первый уровень вложенности, дочерняя для 2"));
+        service.create(properties("2.2.1", "2.2", "Территория 2.2.1", "Второй уровень вложенности, дочерняя для 2.2"));
+        service.create(properties("2.2.2", "2.2", "Территория 2.2.2", "Второй уровень вложенности, дочерняя для 2.2"));
+        service.create(properties("2.3", "2", "Территория 2.3", "Первый уровень вложенности, дочерняя для 2"));
+        service.create(properties("3", null, "Территория 3", "Верхний уровень"));
+        service.create(properties("3.1", "3", "Территория 3.1", "Первый уровень вложенности, дочерняя для 3"));
+        service.create(properties("3.2", "3", "Территория 3.2", "Первый уровень вложенности, дочерняя для 3"));
+        service.create(properties("4", null, "Территория 4", "Верхний уровень"));
     }
 
-    private TerritoryProperties generate(final String code,
-                                         final String ascendantCode,
-                                         final String title,
-                                         final String description) {
+    private TerritoryProperties properties(final String code,
+                                           final String ascendantCode,
+                                           final String title,
+                                           final String description) {
         final TerritoryProperties properties = new TerritoryProperties();
         properties.setCode(code);
         properties.setAscendantCode(ascendantCode);
